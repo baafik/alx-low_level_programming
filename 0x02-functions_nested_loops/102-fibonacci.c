10e94cb90d8587d27ab45ce4c57eb879c1ef91bc
baafik <baafik05@gmail.com>
2022-03-16 22:22:20 -0700

    102-fibonacci.c:9

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: Calculates first 50 Fibonacci numbers
- *     *
- *      * Return: Always 0 (success)
- *       */
+ * main - Entry point
+ *
+ * Description: Calculates first 50 Fibonacci numbers
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 		int count;
 			unsigned long fib1 = 0, fib2 = 1, sum;
-
-				for (count = 0; count < 50; count++)
+			
+			for (count = 0; count < 50; count++)
 						{
 									sum = fib1 + fib2;
 											printf("%lu", sum);
 
 													fib1 = fib2;
 															fib2 = sum;
-
-																	if (count == 49)
+															if (count == 49)
 																					printf("\n");
 																			else
-																							printf(", ");
+																				printf(", ");
 																				}
 
 					return (0);
