2029e0f83f5047dd22730be45d0159781379204d
baafik <baafik05@gmail.com>
2022-03-16 21:50:12 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * * print_to_98 - prints all natural numbers from n to 98
- * *
- * * @n: number
- * */
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: number
+ */
 
 void print_to_98(int n)
 {
