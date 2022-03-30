d206fbdc98490b0af744aa353d4bd48e575cba4d
baafik <baafik05@gmail.com>
2022-03-29 18:35:33 -0700

    Initial changes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
@@ -10,24 +10,24 @@ nclude "main.h"
 
 char *_strpbrk(char *s, char *accept)
 {
-int i, j;
-char *p;
+	int i, j;
+	char *p;
 
-i = 0;
-while (s[i] != '\0')
-{
-j = 0;
-while (accept[j] != '\0')
-{
-if (accept[j] == s[i])
-{
-p = &s[i];
-return (p);
-}
-j++;
-}
-i++;
-}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+	while (accept[j] != '\0')
+	{
+		if (accept[j] == s[i])
+	{
+		p = &s[i];
+		return (p);
+	}
+		j++;
+	}
+		i++;
+	}
 
-return (0);
+	return (0);
 }
